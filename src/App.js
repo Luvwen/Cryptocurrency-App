b7751c810108a489cada1a325eb72f561bffe531
Santiago <santiagobalino@hotmail.com>
2022-03-10 14:34:27 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,56 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+
+import { CryptoHome } from './components/CryptoHome';
+import { CryptoInfo } from './components/CryptoInfo';
+import { Cryptos } from './components/Cryptos';
+import { Exchanges } from './components/Exchanges';
+import { Footer } from './components/Footer';
+import { Navbar } from './components/Navbar';
+import { News } from './components/News';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div>
+      <Navbar />
+      <div>
+        <Routes>
+          <Route exact path='/' element={<CryptoHome />} />
+
+          <Route exact path='/cryptocurrencies' element={<Cryptos />} />
+
+          <Route exact path='/exchanges' element={<Exchanges />} />
+
+          <Route exact path='/news' element={<News />} />
+
+          <Route exact path='/cryptoinfo/:cryptoId' element={<CryptoInfo />} />
+
+          <Route path='/' element={<CryptoHome />} />
+        </Routes>
+      </div>
+      <Footer />
     </div>
   );
 }
