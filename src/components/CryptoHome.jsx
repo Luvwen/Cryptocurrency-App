3fb1cd9486dfc54b25ab95adb29c3e12d2210f19
Santiago <santiagobalino@hotmail.com>
2022-10-20 03:32:57 -0300

    Add conditional chaining in CryptoHome component

--- src/components/CryptoHome.jsx
+++ src/components/CryptoHome.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import millify from 'millify';
+import React from 'react'
+import millify from 'millify'
 
-import { useGetCryptosQuery } from '../services/cryptoApi';
-import { Cryptos } from './Cryptos';
-import { Link } from 'react-router-dom';
-import { News } from './News';
+import { useGetCryptosQuery } from '../services/cryptoApi'
+import { Cryptos } from './Cryptos'
+import { Link } from 'react-router-dom'
+import { News } from './News'
 
 export const CryptoHome = () => {
-  const { data, isFetching } = useGetCryptosQuery(10);
+  const { data, isFetching } = useGetCryptosQuery(10)
 
-  const globalCryptoStats = data?.data?.stats;
+  const globalCryptoStats = data?.data?.stats
 
-  if (isFetching) return 'loading...';
+  if (isFetching) return 'loading...'
 
   return (
     <div className='crypto-container'>
@@ -19,35 +19,35 @@ export const CryptoHome = () => {
       <div className='crypto-wrapper'>
         <div className='crypto'>
           <p className='crypto__subtitle'>Total Cryptocurrencies</p>
-          <p className='crypto__stats'>{globalCryptoStats.total}</p>
+          <p className='crypto__stats'>{globalCryptoStats?.total}</p>
         </div>
         <div className='crypto'>
           <p className='crypto__subtitle'>Total Exchanges</p>
           <p className='crypto__stats'>
-            {millify(globalCryptoStats.totalExchanges)}
+            {millify(globalCryptoStats?.totalExchanges)}
           </p>
         </div>
         <div className='crypto'>
           <p className='crypto__subtitle'>Total Market Cap</p>
           <p className='crypto__stats'>
-            {millify(globalCryptoStats.totalMarketCap)}
+            {millify(globalCryptoStats?.totalMarketCap)}
           </p>
         </div>
         <div className='crypto'>
           <p className='crypto__subtitle'>Total 24h Volume</p>
           <p className='crypto__stats'>
-            {millify(globalCryptoStats.total24hVolume)}
+            {millify(globalCryptoStats?.total24hVolume)}
           </p>
         </div>
         <div className='crypto'>
           <p className='crypto__subtitle'>Total Markets</p>
           <p className='crypto__stats'>
-            {millify(globalCryptoStats.totalMarkets)}
+            {millify(globalCryptoStats?.totalMarkets)}
           </p>
         </div>
         <div className='crypto'>
           <p className='crypto__subtitle'>Total cryptocurrencies</p>
-          <p className='crypto__stats'>{globalCryptoStats.total}</p>
+          <p className='crypto__stats'>{globalCryptoStats?.total}</p>
         </div>
       </div>
       <div className='cryptos-list'>
@@ -65,5 +65,5 @@ export const CryptoHome = () => {
       </div>
       <News amount />
     </div>
-  );
-};
+  )
+}
